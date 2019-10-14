c12b73f3fd4e088abdecf711fa347b2c478152dc
Aditya Pramana <mirror.ojoakua@gmail.com>
2019-10-14 12:56:41 +0700

    Added some scripts

---
+++ main/scripts/deploynode1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
+sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
+sudo percona-release setup ps57
+sudo apt-get upgrade -y
+sudo apt-get remove apparmor -y
+sudo apt-get install percona-xtradb-cluster-server-5.7 -y
